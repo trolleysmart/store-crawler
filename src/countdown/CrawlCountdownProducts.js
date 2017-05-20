86b139c6b7d6beb656ca9f451b4d8fcb50560ddc
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-05-20 21:02:27 +1200

    Refactroing

--- src/countdown/CrawlCountdownProducts.js
+++ src/countdown/CrawlCountdownProducts.js
@@ -1,10 +1,11 @@
 // @flow
 
+import { Exception } from 'micro-business-parse-server-common';
 import CountdownWebCrawlerService from './CountdownWebCrawlerService';
 
 const jobName = 'Crawl Countdown Products';
 
-Parse.Cloud.job(jobName, (request, status) => {
+Parse.Cloud.job(jobName, async (request, status) => {
   // eslint-disable-line no-undef
   const log = request.log;
 
@@ -17,14 +18,15 @@ Parse.Cloud.job(jobName, (request, status) => {
     logErrorFunc: message => log.error(message),
   });
 
-  service
-    .crawlProducts()
-    .then(() => {
-      log.info(`The job ${jobName} completed successfully.`);
-      status.success(`The job ${jobName} completed successfully.`);
-    })
-    .catch((error) => {
-      log.error(`The job ${jobName} ended in error. Error: ${JSON.stringify(error)}`);
-      status.error(`The job ${jobName} ended in error. Error: ${JSON.stringify(error)}`);
-    });
+  try {
+    await service.crawlProducts();
+
+    log.info(`The job ${jobName} completed successfully.`);
+    status.success(`The job ${jobName} completed successfully.`);
+  } catch (ex) {
+    const errorMessage = ex instanceof Exception ? ex.getErrorMessage() : ex;
+
+    log.error(`The job ${jobName} ended in error. Error: ${errorMessage}`);
+    status.error(`The job ${jobName} ended in error. Error: ${errorMessage}`);
+  }
 });
