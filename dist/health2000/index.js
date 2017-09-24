b0b6a3035fb3b29b75fc9dbf638dbeb98dcd09b3
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-09-24 22:38:56 +1300

    Added ist files

---
+++ dist/health2000/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+});
+exports.Health2000WebCrawlerService = undefined;
+
+var _Health2000WebCrawlerService2 = require('./Health2000WebCrawlerService');
+
+var _Health2000WebCrawlerService3 = _interopRequireDefault(_Health2000WebCrawlerService2);
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+exports.Health2000WebCrawlerService = _Health2000WebCrawlerService3.default;
